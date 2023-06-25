8e5d72341543cea7202acfc831429a3c762609b3
Erickleaner <102208544+Erickleaner@users.noreply.github.com>
2023-06-25 14:28:40 +0800

    submit from wyf

--- back-end/src/main/resources/sql/teacher.sql
+++ back-end/src/main/resources/sql/teacher.sql
@@ -2,7 +2,9 @@ create table teacher
 (
     teacher_id int         not null
         primary key,
-    name       varchar(50) null
+    name       varchar(50) null,
+    power      varchar(50) null
 );
 
-INSERT INTO hmk_manager.teacher (teacher_id, name) VALUES (1, '张三');
+INSERT INTO hmk_manager.teacher (teacher_id, name, power) VALUES (1, '张三', 'special');
+INSERT INTO hmk_manager.teacher (teacher_id, name, power) VALUES (2, '李四', 'normal');
